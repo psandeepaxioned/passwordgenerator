c4449b7d1d83991d524f284b94ddbbc940c978df
psandeepaxioned <vikasb@axioned.com>
2022-07-06 17:26:54 +0530

    working on js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 /* Author: 
 
 */
+const generatedPassword = document.querySelector('.generated-password');
+console.log(generatedPassword);
+const copy = document.querySelector('.icon');
+const length = document.querySelector('.length');
+const lowercase = document.querySelector('.lowercase');
+const uppercase = document.querySelector('.uppercase');
+const symbols = document.querySelector('.symbols');
+const number = document.querySelector('.number');
 
 
 
